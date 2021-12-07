cfc67ae3c89cc6b95333ec81ff247cb14c942eb5
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-12-07 15:27:22 +0530

    Test.Analyse Mood for given Statement

--- src/com/MoodAnalyser/MoodAnalyser.java
+++ src/com/MoodAnalyser/MoodAnalyser.java
@@ -1,8 +1,9 @@
 package com.MoodAnalyser;
 
 public class MoodAnalyser {
-
+                    //Test Case 1.1
 	public String Analyse(String msgs) {
+		if(msgs.contains("Sad"))
 		System.out.println(msgs);
 		return "sad";
 	}
--- src/com/MoodAnalyser/MoodAnalyserMain.java
+++ src/com/MoodAnalyser/MoodAnalyserMain.java
@@ -5,10 +5,10 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class MoodAnalyserMain {
-    @Test
+    @Test        //Test Case 1.1
 	public void MoodTest() {
     	MoodAnalyser analysemood = new MoodAnalyser();
-    	String mood = analysemood.Analyse(" Write message");
+    	String mood = analysemood.Analyse("Im in Sad Mood");
     	assertEquals("sad", mood);
     }
 }
