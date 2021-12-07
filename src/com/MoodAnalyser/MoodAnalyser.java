f6cbc7d53a3dc1ec744f47282ed1e3dd12c87cdd
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-12-07 15:37:50 +0530

    Test.Analyse Happy Mood For Statement

--- src/com/MoodAnalyser/MoodAnalyser.java
+++ src/com/MoodAnalyser/MoodAnalyser.java
@@ -1,10 +1,15 @@
 package com.MoodAnalyser;
 
 public class MoodAnalyser {
-                    //Test Case 1.1
+                    //Test Case 1.2
 	public String Analyse(String msgs) {
-		if(msgs.contains("Sad"))
-		System.out.println(msgs);
-		return "sad";
+		if(msgs.contains("Sad")) {
+	    	 System.out.println(msgs);
+			return "sad";
+		}
+	    else {
+    	 System.out.println(msgs);
+			return "happy";
+	   }
 	}
 }
--- src/com/MoodAnalyser/MoodAnalyserMain.java
+++ src/com/MoodAnalyser/MoodAnalyserMain.java
@@ -5,10 +5,16 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class MoodAnalyserMain {
-    @Test        //Test Case 1.1
-	public void MoodTest() {
+    @Test        //Test Case 1.2
+	public void MoodSad() {
     	MoodAnalyser analysemood = new MoodAnalyser();
     	String mood = analysemood.Analyse("Im in Sad Mood");
     	assertEquals("sad", mood);
     }
+    @Test        
+   	public void MooHappy() {
+       	MoodAnalyser analysemood = new MoodAnalyser();
+       	String mood = analysemood.Analyse("Im in Any Mood");
+       	assertEquals("happy", mood);
+       }
 }
